c6a15b60c702b4f3c7043133282cb37ac27736cc
Josh Emerson <josh.j.emerson@gmail.com>
2013-03-12 17:01:04 +0000

    Add GitHub IPs to services script

--- gitpull/index.php
+++ gitpull/index.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 $ip = getenv(REMOTE_ADDR);
-if ($ip == '63.246.22.222') {
+$whitelist = array('54.235.183.49', '54.235.183.23', '54.235.118.251', '54.235.120.57', '54.235.120.61', '54.235.120.62', '63.246.22.222');
+if (in_array($ip, $whitelist)) {
     $path = $_GET['path'];
     exec("cd ~/$path && git pull");
     error_log("Repo at ~/$path updated.",0);
