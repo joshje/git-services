39d69eb3641b326f5d90b40acd381b5532928f90
Clearleft Server <server@clearleft.com>
2013-03-13 10:34:18 +0000

    Changes for Clearleft server

--- gitpull/index.php
+++ gitpull/index.php
@@ -3,6 +3,5 @@ $ip = getenv(REMOTE_ADDR);
 $whitelist = array('54.235.183.49', '54.235.183.23', '54.235.118.251', '54.235.120.57', '54.235.120.61', '54.235.120.62', '63.246.22.222');
 if (in_array($ip, $whitelist)) {
     $path = $_GET['path'];
-    exec("cd ~/$path && git pull");
-    error_log("Repo at ~/$path updated.",0);
+    exec("cd ~/$path && git pull origin master");
 }
